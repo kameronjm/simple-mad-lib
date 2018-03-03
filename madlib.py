f951dda219ac1448a082beab25fb42767636398d
kameronjm <kameronjmyers@gmail.com>
2018-03-03 11:14:39 -0500

    initial upload

---
+++ madlib.py
@@ -0,0 +1,19 @@
+noun1 = raw_input("Write a noun ")
+verb1 = raw_input("Write a verb ")
+liquid = raw_input("Write a kind of liquid ")
+verb2 = raw_input("Write a verb ending in '-ed' ")
+noun2 = raw_input("Write a noun ")
+verb3 = raw_input("Write a verb ending in '-ing/' ")
+verb4 = raw_input("Write a verb ")
+adjective1 = raw_input("Write an adjective ")
+noun3 = raw_input("Write a noun ")
+adjective2 = raw_input("Write an adjective ")
+print "Jack and Jill went up the %s, " \
+"To %s a pail of %s; " \
+"Jack %s down, and broke his %s, " \
+"And Jill came %s after. " \
+"Then up Jack got and off did %s, " \
+"As %s as he could caper, " \
+"To old Dame Dob, who patched his %s " \
+"With %s and %s paper." % (noun1, verb1, liquid, verb2, noun2, verb3, verb4, adjective1, noun3, adjective2)
+
