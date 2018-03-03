325f88e9322b628fa72311628ebe684ebfb5f4ca
kameronjm <kameronjmyers@gmail.com>
2018-03-03 12:04:36 -0500

    bug fixing and different methods

--- madlib.py
+++ madlib.py
@@ -1,19 +1,18 @@
-noun1 = raw_input("Write a noun ")
-verb1 = raw_input("Write a verb ")
-liquid = raw_input("Write a kind of liquid ")
-verb2 = raw_input("Write a verb ending in '-ed' ")
-noun2 = raw_input("Write a noun ")
-verb3 = raw_input("Write a verb ending in '-ing/' ")
-verb4 = raw_input("Write a verb ")
-adjective1 = raw_input("Write an adjective ")
-noun3 = raw_input("Write a noun ")
-adjective2 = raw_input("Write an adjective ")
-print "Jack and Jill went up the %s, " \
-"To %s a pail of %s; " \
-"Jack %s down, and broke his %s, " \
-"And Jill came %s after. " \
-"Then up Jack got and off did %s, " \
-"As %s as he could caper, " \
-"To old Dame Dob, who patched his %s " \
-"With %s and %s paper." % (noun1, verb1, liquid, verb2, noun2, verb3, verb4, adjective1, noun3, adjective2)
-
+noun1 = input("Write a noun ")
+verb1 = input("Write a verb ")
+liquid = input("Write a kind of liquid ")
+verb2 = input("Write a verb ending in '-ed' ")
+noun2 = input("Write a noun ")
+verb3 = input("Write a verb ending in '-ing/' ")
+verb4 = input("Write a verb ")
+adjective1 = input("Write an adjective ")
+noun3 = input("Write a noun ")
+adjective2 = input("Write an adjective ")
+print ("Jack and Jill went up the", noun1)
+print ("To", verb1, "a pail of", liquid)
+print ("Jack", verb2, "down, and broke his", noun2)
+print ("And Jill came", verb3, "after.")
+print ("Then up Jack got and off did", verb4,)
+print ("As", adjective1, "as he could caper,") 
+print ("To old Dame Dob, who patched his ", noun3)
+print ("With", noun2, "and", adjective2, "paper.")
